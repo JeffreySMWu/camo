5b9a9e6c36d924e624f26d958b4cdcf80c49f865
JeffreySMWu <50734923+JeffreySMWu@users.noreply.github.com>
2019-06-13 16:39:43 +0800

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,57 @@
+DeviceOrientation = function (_target) {
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener('deviceorientation', function (event) {
+            var alpha = event.alpha,
+                beta = event.beta,
+                gamma = event.gamma,
+                a = Math.round(alpha),
+                b = Math.round(beta),
+                g = Math.round(gamma);
+
+            //   var a = document.getElementById('alpha'),
+            //   		b = document.getElementById('beta'),
+            //   		g = document.getElementById('gamma'),
+
+            // a = Math.round(alpha);
+            // b = Math.round(beta);
+            // g = Math.round(gamma);
+            console.log("X:" + g);
+            console.log("Y:" + b);
+            // TweenMax.set(_target, {
+            //     y: -b / 1.2
+            // });
+            // TweenMax.set(_target, {
+            //     x: -g * 1.5
+            // });
+            if (b < 90 && b > -90) {
+                TweenMax.set(_target, {
+                    y: -b / 2
+                });
+            }
+            if (g < 90 && g > -90) {
+                TweenMax.set(_target, {
+                    x: -g * 2
+                });
+            }
+        }, false);
+    } else {
+        // document.querySelector('body').innerHTML = '雿删��讛汗�膥銝齿𣈲�螱���';
+    }
+}
+
+var new_device = new DeviceOrientation($("#layer"));
+
+
+var x, y;
+var $dot = $("#brush");
+
+$(document).mousemove( function(e) {
+  //Get the 'container' offset relative to the document:
+  offset = $('body').offset();
+  //Track the mouse position:
+  x = e.pageX - offset.left;
+  y = e.pageY - offset.top;
+  //Move the 'dot':
+  $dot.css({"left": x});
+  $dot.css({"top": y});
+});
