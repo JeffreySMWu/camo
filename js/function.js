90663ee92d680479d5662b005ed5e6433c2e115c
JeffreySMWu <50734923+JeffreySMWu@users.noreply.github.com>
2019-06-13 17:39:52 +0800

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -6,32 +6,13 @@ DeviceOrientation = function (_target) {
                 gamma = event.gamma,
                 a = Math.round(alpha),
                 b = Math.round(beta),
-                g = Math.round(gamma);
+                g = Math.round(gamma),
+                origin = 0;
 
-            //   var a = document.getElementById('alpha'),
-            //   		b = document.getElementById('beta'),
-            //   		g = document.getElementById('gamma'),
-
-            // a = Math.round(alpha);
-            // b = Math.round(beta);
-            // g = Math.round(gamma);
             console.log("X:" + g);
             console.log("Y:" + b);
-            // TweenMax.set(_target, {
-            //     y: -b / 1.2
-            // });
-            // TweenMax.set(_target, {
-            //     x: -g * 1.5
-            // });
-            if (b < 90 && b > -90) {
-                TweenMax.set(_target, {
-                    y: -b / 2
-                });
-            }
-            if (g < 90 && g > -90) {
-                TweenMax.set(_target, {
-                    x: -g * 2
-                });
+            if(b > origin ) {
+                origin = b;
             }
         }, false);
     } else {
